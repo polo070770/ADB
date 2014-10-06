14c14c1e3e12cb4a86dfc9294c4e647269b51657
polo070770 <polo070770@gmail.com>
2014-10-06 11:43:19 +0200

    modificacio headers

--- adb_privalia/src/com/model/Producte.java
+++ adb_privalia/src/com/model/Producte.java
@@ -1,13 +1,8 @@
 package com.model;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
- * @author maikell
+ * @author maikel
  */
 public abstract class Producte {
     
