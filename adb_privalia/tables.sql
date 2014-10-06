5531b2774dcf5ff12d35e1dea61f28b026dd33ea
polo070770 <polo070770@gmail.com>
2014-10-06 10:49:32 +0200

    bd

---
+++ adb_privalia/tables.sql
@@ -0,0 +1,90 @@
+DROP TABLE IF EXISTS campanya;
+DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS linea_factura;
+DROP TABLE IF EXISTS dades_entrega;
+DROP TABLE IF EXISTS familia;
+DROP TABLE IF EXISTS subfamilia;
+DROP TABLE IF EXISTS article;
+
+CREATE TABLE familia(
+  id_familia INTEGER PRIMARY KEY NOT NULL,
+  nom VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE subfamilia(
+
+  id_subfamilia INTEGER PRIMARY KEY NOT NULL,
+  nom VARCHAR(50) NOT NULL,
+  
+  id_fam INTEGER,
+  
+  FOREIGN KEY(id_fam) REFERENCES familia(id_familia) 
+  
+);
+
+CREATE TABLE campanya(
+
+	id_campanya INTEGER PRIMARY KEY NOT NULL,
+
+	nom_campanya VARCHAR(30) NOT NULL,
+
+	data_inici DATE NOT NULL,
+	data_fi DATE NOT NULL,
+
+	marca VARCHAR(20),
+	num_articles INTEGER,
+
+	total_facturat FLOAT
+
+);
+
+CREATE TABLE article(
+  id_article INTEGER PRIMARY KEY NOT NULL,
+  nom VARCHAR(50) NOT NULL,
+  talla FLOAT,
+  color VARCHAR(20),
+  marca VARCHAR(20),
+  preu FLOAT,
+  
+  id_subfam INTEGER,
+  id_campanya INTEGER,
+  
+  FOREIGN KEY(id_subfam) REFERENCES subfamilia(id_subfamilia),
+  FOREIGN KEY(id_campanya) REFERENCES campanya(id_campanya)
+);
+
+CREATE TABLE linea_factura(
+
+	id_client VARCHAR(10),
+	id_article INTEGER,
+	
+	import_article FLOAT,  
+
+	FOREIGN KEY(id_article) REFERENCES article(id_article),
+	FOREIGN KEY(id_client) REFERENCES client(email)
+);
+
+CREATE TABLE client(
+
+	email VARCHAR(50) PRIMARY KEY NOT NULL,
+
+	nom VARCHAR(20) NOT NULL,
+	cognom VARCHAR(50) NOT NULL,
+	telefon VARCHAR(9),
+
+	import_total FLOAT
+  
+);
+
+CREATE TABLE dades_entrega(
+
+  carrer VARCHAR(30),
+  poblacio VARCHAR(30),
+  provincia VARCHAR(30),
+  codi_postal VARCHAR(5),
+  pais VARCHAR(30),
+  
+  id_client VARCHAR(9),
+  
+  FOREIGN KEY(id_client) REFERENCES client(email)
+);
