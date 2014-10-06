f68b35242a79d07faa5af90473b8efefd9c3ab56
polo070770 <polo070770@gmail.com>
2014-10-06 11:33:27 +0200

    producte

---
+++ adb_privalia/src/com/model/Producte.java
@@ -0,0 +1,14 @@
+package com.model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author maikell
+ */
+public abstract class Producte {
+    
+}
