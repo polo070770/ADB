bb0a2c08ae0eca9a5a12c3ce61bfa98ff83fd229
orquidea1992 <tian.superboy.lan50@gmail.com>
2014-10-12 18:02:59 +0200

    disseny arreglat, BD versio per app feta, algunes taules modificades, insercions quasi fetes

--- adb_privalia/tables.sql
+++ adb_privalia/tables.sql
@@ -13,39 +13,31 @@ CREATE TABLE familia(
 );
 
 CREATE TABLE subfamilia(
-
   id_subfamilia INTEGER PRIMARY KEY NOT NULL,
   nom VARCHAR(50) NOT NULL,
   
   id_fam INTEGER,
-  
   FOREIGN KEY(id_fam) REFERENCES familia(id_familia) 
-  
 );
 
 CREATE TABLE campanya(
-
 	id_campanya INTEGER PRIMARY KEY NOT NULL,
 	nom_campanya VARCHAR(30) NOT NULL,
 	marca VARCHAR(20),
 	num_articles INTEGER,
-	total_facturat FLOAT
-  data_inici DATE NOT NULL,
-  data_fi DATE NOT NULL,
-
+	total_facturat FLOAT,
+  	data_inici DATE NOT NULL,
+  	data_fi DATE NOT NULL
 );
 
 CREATE TABLE producte(
-
   id_producte INTEGER PRIMARY KEY NOT NULL,
   nom VARCHAR(50),
   marca VARCHAR(20),
-  num_articles INTEGER,
-
+  num_articles INTEGER
 );
 
 CREATE TABLE article(
-
   id_article INTEGER PRIMARY KEY NOT NULL,
   talla FLOAT,
   color VARCHAR(20),
@@ -53,41 +45,37 @@ CREATE TABLE article(
   
   id_subfam INTEGER,
   id_campanya INTEGER,  
-  
+  id_producte INTEGER,
   FOREIGN KEY(id_subfam) REFERENCES subfamilia(id_subfamilia),
-  FOREIGN KEY(id_campanya) REFERENCES campanya(id_campanya)
+  FOREIGN KEY(id_campanya) REFERENCES campanya(id_campanya),
+  FOREIGN KEY(id_producte) REFERENCES producte(id_producte)
 );
 
 CREATE TABLE linea_factura(
-
-	id_client VARCHAR(10),
+	id_factura INTEGER PRIMARY KEY NOT NULL,
+	id_client INTEGER,
 	id_article INTEGER,
 
 	FOREIGN KEY(id_article) REFERENCES article(id_article),
-	FOREIGN KEY(id_client) REFERENCES client(email)
+	FOREIGN KEY(id_client) REFERENCES client(id_client)
 );
 
 CREATE TABLE client(
-
-	email VARCHAR(50) PRIMARY KEY NOT NULL,
-
+	id_client INTEGER PRIMARY KEY NOT NULL,
+	email VARCHAR(50) NOT NULL,
 	nom VARCHAR(20) NOT NULL,
 	cognom VARCHAR(50) NOT NULL,
 	telefon VARCHAR(9),
-
 	import_total FLOAT
-  
 );
 
 CREATE TABLE dades_entrega(
-
   carrer VARCHAR(30),
   ciudad VARCHAR(30),
   comunidad VARCHAR(30),
   codi_postal VARCHAR(5),
   pais VARCHAR(30),
   
-  id_client VARCHAR(50),
-  
-  FOREIGN KEY(id_client) REFERENCES client(email)
+  id_client INTEGER,
+  FOREIGN KEY(id_client) REFERENCES client(id_client)
 );
